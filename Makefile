7d7846cc4c81ab17a39eafa6a315974bfa71a859
myp <3144529579@qq.com>
2024-05-25 16:08:53 +0800

    lab1

--- Makefile
+++ Makefile
@@ -149,7 +149,10 @@ UPROGS=\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
-
+	$U/_sleep\
+	$U/_pingpong\
+	$U/_Primes\
+	$U/_find\
 
 ifeq ($(LAB),syscall)
 UPROGS += \
---
+++ user/pingpong.c
@@ -0,0 +1,74 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+#define RD 0
+#define WR 1
+
+int main(int argc, char const *argv[])
+{
+    int fd_p2c[2], fd_c2p[2];
+    pipe(fd_c2p);
+    pipe(fd_p2c);
+    char buf = 'a';
+
+    int pid = fork();
+    int exit_state = 0;
+
+    if (pid < 0)
+    {
+        fprintf(2, "fork() error!\n");
+        close(fd_c2p[RD]);
+        close(fd_c2p[WR]);
+        close(fd_p2c[RD]);
+        close(fd_p2c[WR]);
+
+        exit(1);
+    } else if (pid == 0) {
+        close(fd_p2c[WR]);
+        close(fd_c2p[RD]);
+
+        if (read(fd_p2c[RD], &buf, sizeof(char)) != sizeof(char))
+        {
+            fprintf(2, "child read() error!\n");
+            exit_state = 1;
+        } else {
+            fprintf(1, "%d: received ping\n", getpid());
+        }
+        
+        if (write(fd_c2p[WR], &buf, sizeof(char)) != sizeof(char))
+        {
+            fprintf(2, "child write() error!\n");
+            exit_state = 1;
+        }
+
+        close(fd_p2c[RD]);
+        close(fd_c2p[WR]);
+
+        exit(exit_state);
+    } else {
+        close(fd_c2p[WR]);
+        close(fd_p2c[RD]);
+
+        if (write(fd_p2c[WR], &buf, sizeof(char)) != sizeof(char))
+        {
+            fprintf(2, "parent write() error!\n");
+            exit_state = 1;
+        }
+
+
+        if (read(fd_c2p[RD], &buf, sizeof(char)) != sizeof(char))
+        {
+            fprintf(2, "parent read() error!\n");
+            exit_state = 1;
+        } else {
+            fprintf(1, "%d: received pong\n", getpid());
+        }
+
+        
+        close(fd_p2c[WR]);
+        close(fd_c2p[RD]);
+        
+
+        exit(exit_state);
+    }
+}
